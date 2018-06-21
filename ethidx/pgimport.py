d0f30f307b68c2f043c2e26a69b329bd19572383
Ralf Schmitt <ralf@systemexit.de>
2018-06-21 17:04:00 +0200

    enrich events with information from blocks
    we need the timestamp for each event, which is only stored in the block.
    
    So, after fetching events, we fetch each block by block number that contains an
    event.
    
    Strictly speaking we could have one or multiple chain reorgs while fetching
    those blocks, so we compare the block hash with the one stored in the event and
    bail out if they don't match.
    
    I had already been introducing a new table 'blocks', which would hold some basic
    information about blocks. But then decided against it, cause it's a bit easier
    to implement this way, i.e. with a single table.
    
    Also, we now use psycopg2.extras.RealDictCursor. This makes psycopg2 give us
    results as a dictionary (instead of a tuple).

--- ethidx/pgimport.py
+++ ethidx/pgimport.py
@@ -3,37 +3,42 @@
 import json
 from web3 import Web3
 import psycopg2
+import psycopg2.extras
 import binascii
-
+import logging
 from ethidx import logdecode
 
 
+logger = logging.getLogger(__name__)
 # https://github.com/ethereum/wiki/wiki/JavaScript-API#web3ethgettransactionreceipt
 
 
-def get_logs(addresses):
-
-    web3 = Web3(
-        Web3.HTTPProvider("http://127.0.0.1:8545", request_kwargs={"timeout": 60})
-    )
-
+def get_logs(web3, addresses):
     return web3.eth.getLogs(
         {"fromBlock": "0x0", "toBlock": "latest", "address": addresses}
     )
 
 
-def get_events():
+def get_events(web3):
     a2abi = logdecode.build_address_to_abi_dict(
         json.load(open("addresses.json")), json.load(open("contracts.json"))
     )
     topic_index = logdecode.TopicIndex(a2abi)
-
-    for x in get_logs(topic_index.addresses):
-        yield topic_index.decode_log(x)
+    return [topic_index.decode_log(x) for x in get_logs(web3, topic_index.addresses)]
 
 
 def hexlify(d):
     return "0x" + binascii.hexlify(d).decode()
+
+
+def insert_blocks(conn, blocks):
+    with conn.cursor() as cur:
+        for b in blocks:
+            cur.execute(
+                """INSERT INTO blocks (blockNumber, blockHash, timestamp)
+                           VALUES (%s, %s, %s)""",
+                (b["number"], hexlify(b["hash"]), b["timestamp"]),
+            )
 
 
 def insert_events(conn, events):
@@ -47,8 +52,9 @@ def insert_events(conn, events):
                                        args,
                                        blockHash,
                                        transactionIndex,
-                                       logIndex)
-                               VALUES (%s, %s, %s, %s, %s, %s, %s, %s)""",
+                                       logIndex,
+                                       timestamp)
+                               VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)""",
                 (
                     hexlify(x["log"]["transactionHash"]),
                     x["log"]["blockNumber"],
@@ -58,13 +64,43 @@ def insert_events(conn, events):
                     hexlify(x["log"]["blockHash"]),
                     x["log"]["transactionIndex"],
                     x["log"]["logIndex"],
+                    x["timestamp"],
                 ),
             )
 
 
+def event_blocknumbers(events):
+    """given a list of events returns the block numbers containing events"""
+    return {ev["log"]["blockNumber"] for ev in events}
+
+
+def connect(dsn):
+    return psycopg2.connect(dsn, cursor_factory=psycopg2.extras.RealDictCursor)
+
+
+def enrich_events(events, blocks):
+    block_by_number = {b["number"]: b for b in blocks}
+    for e in events:
+        blocknumber = e["log"]["blockNumber"]
+        block = block_by_number[blocknumber]
+        if block["hash"] != e["log"]["blockHash"]:
+            raise RuntimeError("bad hash! chain reorg?")
+        e["timestamp"] = block["timestamp"]
+
+
 def main():
-    events = get_events()
-    with psycopg2.connect("") as conn:  # we rely on the PG* variables to be set
+    logging.basicConfig(level=logging.INFO)
+    web3 = Web3(
+        Web3.HTTPProvider("http://127.0.0.1:8545", request_kwargs={"timeout": 60})
+    )
+
+    events = get_events(web3)
+    blocknumbers = event_blocknumbers(events)
+    logger.info("got %s events in %s blocks", len(events), len(blocknumbers))
+    blocks = [web3.eth.getBlock(x) for x in blocknumbers]
+    enrich_events(events, blocks)
+
+    with connect("") as conn:  # we rely on the PG* variables to be set
         insert_events(conn, events)
 
 

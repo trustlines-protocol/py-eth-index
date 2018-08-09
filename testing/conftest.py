904e4c99e678a2952d2a9f65e7212452771c39eb
Ralf Schmitt <ralf@systemexit.de>
2018-08-09 13:11:12 +0200

    add find_tables, check that tables are created

--- testing/conftest.py
+++ testing/conftest.py
@@ -1,9 +1,11 @@
 import os
 import testing.postgresql
 import pytest
+import psycopg2
+import psycopg2.extras
 
 
-@pytest.fixture(scope="module")
+@pytest.fixture(scope="session")
 def postgresql_factory():
     # Generate Postgresql class which shares the generated database
     factory = testing.postgresql.PostgresqlFactory(cache_initialized_db=True)
@@ -26,3 +28,10 @@ def postgresql(postgresql_factory):
 @pytest.fixture
 def postgresql_dsn(postgresql):
     return postgresql.dsn()
+
+
+@pytest.fixture
+def conn(postgresql_dsn):
+    return psycopg2.connect(
+        **postgresql_dsn, cursor_factory=psycopg2.extras.RealDictCursor
+    )
--- testing/test_createtables.py
+++ testing/test_createtables.py
@@ -1,7 +1,18 @@
 import os
 
 
-def test_createtables(postgresql_dsn):
-    print("DSN:", postgresql_dsn)
+def find_tables(conn):
+    """return a list of tables"""
+    with conn.cursor() as cur:
+        cur.execute(
+            "select table_name from information_schema.tables where table_schema='public'"
+        )
+        return cur.fetchall()
+
+
+def test_createtables(conn):
     err = os.system("ethindex createtables")
     assert err == 0
+    tables = find_tables(conn)
+    print("FOUND", tables)
+    assert len(tables) >= 3

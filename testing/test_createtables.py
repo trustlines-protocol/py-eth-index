af3dd1124971078f1d67c5130fe2d9c9c056b0ce
Ralf Schmitt <ralf@systemexit.de>
2018-08-09 13:21:20 +0200

    test that 'ethindex droptables' works

--- testing/test_createtables.py
+++ testing/test_createtables.py
@@ -16,3 +16,24 @@ def test_createtables(conn):
     tables = find_tables(conn)
     print("FOUND", tables)
     assert len(tables) >= 3
+
+
+def test_droptables_without_force(conn):
+    """droptables require the user to pass --force"""
+    err = os.system("ethindex createtables")
+    assert err == 0
+    tables_before = find_tables(conn)
+    err = os.system("ethindex droptables")
+    assert err == 256  # exit code 1
+    tables_after = find_tables(conn)
+    assert tables_before == tables_after
+
+
+def test_droptables_with_force(conn):
+    err = os.system("ethindex createtables")
+    assert err == 0
+    err = os.system("ethindex droptables --force")
+    assert err == 0
+
+    tables = find_tables(conn)
+    assert tables == []

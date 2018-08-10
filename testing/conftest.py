ccf02cbd2d3d5dc4ae20e1fe0139845133c610a1
Ralf Schmitt <ralf@systemexit.de>
2018-08-10 12:43:38 +0200

    add eth-tester
    We need this for testing interaction with the blockchain. This adds eth-tester
    together with py-evm as a backend. This introduces lot's of new (indirect)
    dependencies (as can be seen in the constraints.txt file)
    
    Currently we don't deploy any of our contracts. Instead we test that eth_getLogs
    works. I ran into a regresssion with that function while trying to update to
    web3 4.5.0. See https://github.com/ethereum/web3.py/issues/999

--- testing/conftest.py
+++ testing/conftest.py
@@ -3,6 +3,9 @@ import testing.postgresql
 import pytest
 import psycopg2
 import psycopg2.extras
+import eth_tester
+from web3 import Web3
+from web3.providers.eth_tester import EthereumTesterProvider
 
 
 @pytest.fixture(scope="session")
@@ -35,3 +38,18 @@ def conn(postgresql_dsn):
     return psycopg2.connect(
         **postgresql_dsn, cursor_factory=psycopg2.extras.RealDictCursor
     )
+
+
+@pytest.fixture(scope="session")
+def ethereum_tester():
+    """Returns an instance of an Ethereum tester"""
+    tester = eth_tester.EthereumTester(eth_tester.PyEVMBackend())
+    return tester
+
+
+@pytest.fixture
+def web3_eth_tester(ethereum_tester):
+    web3 = Web3(EthereumTesterProvider(ethereum_tester))
+    snapshot = ethereum_tester.take_snapshot()
+    yield web3
+    ethereum_tester.revert_to_snapshot(snapshot)

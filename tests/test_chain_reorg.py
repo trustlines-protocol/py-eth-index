523d4d244c2d280669d33f86b99a47d52b511136
Ralf Schmitt <ralf@systemexit.de>
2018-09-06 15:52:50 +0200

    add a failing test for chain reorgs that shorten the chain
    reorgs may result in a shorter chain since the chain only has to be longer
    difficulty-wise.
    
    Here's a test case that show our current code not being able to handle this.

--- tests/test_chain_reorg.py
+++ tests/test_chain_reorg.py
@@ -1,5 +1,7 @@
 """test that chain reorgs are handled"""
 
+import pytest
+
 
 def fetch_events(conn):
     with conn.cursor() as cur:
@@ -10,7 +12,7 @@ def fetch_events(conn):
 
 
 def test_reorg(testenv, event_emitter, conn, synchronizer):
-    # event_emitter.add_some_tranfer_events adds 3 events increases the value
+    # event_emitter.add_some_tranfer_events adds 3 events and increases the value
     # added by one for each event
 
     event_emitter.add_some_tranfer_events()  # add events with values 0, 1, 2
@@ -27,3 +29,31 @@ def test_reorg(testenv, event_emitter, conn, synchronizer):
     synchronizer.sync_until_current()
     values_after = fetch_events(conn)
     assert values_after == [0, 1, 2, 6, 7, 8, 9, 10, 11]
+
+
+@pytest.mark.xfail
+def test_reorg_shorter_chain(testenv, event_emitter, conn, synchronizer):
+    """test that reorgs that result in a shorter chain are handled
+
+    The chain could get shorter since it only has to be the longest chain difficulty-wise.
+    Not sure if this is only a theoretical problem.
+    """
+    # event_emitter.add_some_tranfer_events adds 3 events and increases the value
+    # added by one for each event
+
+    event_emitter.add_some_tranfer_events()  # add events with values 0, 1, 2
+    snapshot = testenv.ethereum_tester.take_snapshot()
+    event_emitter.add_some_tranfer_events()  # add events with values 3, 4, 5
+    event_emitter.add_some_tranfer_events()  # add events with values 6, 7, 8
+    synchronizer.sync_until_current()
+    values_before = fetch_events(conn)
+    print("values_before", values_before)
+    assert values_before == [0, 1, 2, 3, 4, 5, 6, 7, 8]
+
+    # now reorg the chain
+    testenv.ethereum_tester.revert_to_snapshot(snapshot)
+    event_emitter.add_some_tranfer_events()  # add events with values 9, 10, 11
+    synchronizer.sync_until_current()
+    values_after = fetch_events(conn)
+    print("values_after", values_after)
+    assert values_after == [0, 1, 2, 9, 10, 11]

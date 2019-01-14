1e698ea900f1c634eaf84dc07e4cf9fa2e015a8d
Ralf Schmitt <ralf@systemexit.de>
2019-01-14 01:45:47 +0100

    implement --version switch

--- ethindex/cli.py
+++ ethindex/cli.py
@@ -1,10 +1,22 @@
 import click
+import pkg_resources
+
 import ethindex.pgimport
 
 
-@click.group()
-def cli():
-    pass
+def report_version():
+    click.echo(pkg_resources.get_distribution("eth-index").version)
+
+
+@click.group(invoke_without_command=True)
+@click.option("--version", help="Prints the version of the software", is_flag=True)
+@click.pass_context
+def cli(ctx, version):
+    if version:
+        report_version()
+    elif ctx.invoked_subcommand is None:
+        click.echo(ctx.get_help())
+        ctx.exit()
 
 
 cli.add_command(ethindex.pgimport.importabi)

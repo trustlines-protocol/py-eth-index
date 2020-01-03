dadf0b2a84b51083f9d50546bd0924d6a286a8d2
Ralf Schmitt <ralf@brainbot.com>
2020-01-03 10:55:20 +0100

    Switch to a src based layout
    We did this on our other repos as well.
    
    For more information on source based layout visit the blog posts
    [1][2].
    
    [1] https://blog.ionelmc.ro/2014/05/25/python-packaging/
    [2] https://hynek.me/articles/testing-packaging/

--- setup.py
+++ setup.py
@@ -48,32 +48,10 @@ setup(
     ],
     # What does your project relate to?
     keywords="ethereum indexer database",
-    # You can just specify the packages manually here if your project is
-    # simple. Or you can use find_packages().
-    packages=find_packages(exclude=["contrib", "docs", "tests"]),
-    # Alternatively, if you want to distribute just a my_module.py, uncomment
-    # this:
-    #   py_modules=["my_module"],
-    # List run-time dependencies here.  These will be installed by pip when
-    # your project is installed. For an analysis of "install_requires" vs pip's
-    # requirements files see:
-    # https://packaging.python.org/en/latest/requirements.html
+    packages=find_packages("src"),
+    package_dir={"": "src"},
     install_requires=["web3>=4.4.1", "psycopg2>=2.7", "click", "attrs"],
-    # List additional groups of dependencies here (e.g. development
-    # dependencies). You can install these using the following syntax,
-    # for example:
-    # $ pip install -e .[dev,test]
-    extras_require={},
-    # If there are data files included in your packages that need to be
-    # installed, specify them here.  If using Python 2.6 or less, then these
-    # have to be included in MANIFEST.in as well.
-    package_data={},
     python_requires=">=3.6",
-    # Although 'package_data' is the preferred approach, in some case you may
-    # need to place data files outside of your packages. See:
-    # http://docs.python.org/3.4/distutils/setupscript.html#installing-additional-files # noqa
-    # In this case, 'data_file' will be installed into '<sys.prefix>/my_data'
-    data_files=[],
     entry_points="""
     [console_scripts]
     ethindex=ethindex.cli:cli
